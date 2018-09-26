177d22ac75126517b0e578f5c10b4b758288c22f
Thejesh <thejeshpr@gmail.com>
2018-09-26 12:06:20 +0530

    removed debug mode

--- app.py
+++ app.py
@@ -59,5 +59,6 @@ def parse():
                            title=TITLE)
 
 if __name__ == "__main__":
-    app.run(debug=True)
-    app.run(host='0.0.0.0', port=8080)
+    #app.run(debug=True)
+    #app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=80)
