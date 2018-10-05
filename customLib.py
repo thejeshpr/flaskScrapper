aa67bda04e61fab93e1ee90a59e901a3057c40be
Thejesh <thejeshpr@gmail.com>
2018-10-05 16:07:16 +0530

    Corrected comments in customLib

--- customLib.py
+++ customLib.py
@@ -111,6 +111,7 @@ def process_request():
     # validate the given url in the request and extract url info
     url, scheme, netloc, path, query_param = validate_url(url)
     
+    # Parse the url
     parsed_data = parse_url(url)
     parsed_data['url'] = url
     if request.args.get(RETURN_JSON_QP_NAME) == 'true':
