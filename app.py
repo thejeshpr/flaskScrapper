44f8c988d9155fce9e54f87e66c2e28bf5afba33
Thejesh <thejeshpr@gmail.com>
2018-10-06 16:37:20 +0530

    pep8 code fix in app.py

--- app.py
+++ app.py
@@ -55,6 +55,6 @@ def parse():
 
 
 if __name__ == "__main__":
-    #app.run(debug=True)
-    #app.run(host='0.0.0.0', port=8080)
+    # app.run(debug=True)
+    # app.run(host='0.0.0.0', port=8080)
     app.run(host='0.0.0.0', port=80)
