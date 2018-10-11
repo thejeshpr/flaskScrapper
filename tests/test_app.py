f07d29c03157886a39e67cf80fbf3cb26cce1aba
Thejesh <thejeshpr@gmail.com>
2018-10-11 11:41:18 +0530

    updated testcase

--- tests/test_app.py
+++ tests/test_app.py
@@ -59,8 +59,7 @@ class TestApp(object):
         }
         response = http_get(
             urljoin(TestApp.web_server_url, 'parse'), params=qp)
-        assert ('Unable to parse given url, status code : 404'
-                in response.text)
+        assert 'Unable to parse given url' in response.text
 
     def test_parseValidURLUsingAPI(self):
         """
