3672ab1e9488e96268d53fa7d68848aaaa3ea1ae
Thejesh <thejeshpr@gmail.com>
2018-10-11 16:28:11 +0530

    upadted docstring

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,13 @@
-# content of conftest.py
+"""
+This is configuration for pytest to enable custom tweaks
+"""
 import pytest
 
 
 def pytest_addoption(parser):
+    """
+    Add custom command line params
+    """
     parser.addoption(
         "--test_url", action="store",
         required=True, help="Test URL to test"
@@ -19,14 +24,23 @@ def pytest_addoption(parser):
 
 @pytest.fixture
 def test_url(request):
+    """
+    Make test_url avaiable as pytest config
+    """
     return request.config.getoption("--test_url")
 
 
 @pytest.fixture
 def web_server_port(request):
+    """
+    Make web_server_port avaiable as pytest config
+    """
     return request.config.getoption("--web_server_port")
 
 
 @pytest.fixture
 def web_server_host(request):
+    """
+    Make web_server_host avaiable as pytest config
+    """
     return request.config.getoption("--web_server_host")
